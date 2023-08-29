0ce4df50f61ba92b28916998a78e6f1eb9a534e3
D3FALT404 <63247628+D3FALT404@users.noreply.github.com>
2023-08-29 17:51:20 +0200

    1.0

--- build/lib/pacman.py
+++ build/lib/pacman.py
@@ -3,13 +3,36 @@ import os
 
 @click.command()
 @click.option("-S", "--install", type=str, default="")
-def cli(install):
+@click.option("-Ss", "--search", type=str, default="")
+@click.option("-Syu", "--update", is_flag=True)
+@click.option("-R", "--remove", type=str, default="")
+def cli(install, update, search, remove):
     if install:
-        apps = install.split(" ")
+        click.echo(install)
+        apps = string_to_array(install)
+        click.echo(apps)
         for app in apps:
-            os.system(f"winget install {app} -s winget")
+            os.system(f'winget install "{app}" -s winget --accept-source-agreements')
+    elif remove:
+        apps = string_to_array(remove)
+        for app in apps:
+            os.system(f'winget rm "{app}"')
+    elif search:
+        os.system(f'winget search "{search}"')
+    else:
+        os.system("winget update --all")
+
 
     """Simple program that greets NAME for a total of COUNT times."""
 
+
+def string_to_array(string):
+    if ',' in string:
+        apps = string.split(",")
+    else:
+        apps = string.split(" ")
+    return apps
+
+string_to_array("discord,visual studio community")
 if __name__ == '__main__':
     cli()
--- pacman.py
+++ pacman.py
@@ -5,18 +5,20 @@ import os
 @click.option("-S", "--install", type=str, default="")
 @click.option("-Ss", "--search", type=str, default="")
 @click.option("-Syu", "--update", is_flag=True)
-@click.option("-Rns", "--remove", type=str, default="")
+@click.option("-R", "--remove", type=str, default="")
 def cli(install, update, search, remove):
     if install:
+        click.echo(install)
         apps = string_to_array(install)
+        click.echo(apps)
         for app in apps:
-            os.system(f"winget install {app} -s winget")
+            os.system(f'winget install "{app}" -s winget --accept-source-agreements')
     elif remove:
         apps = string_to_array(remove)
         for app in apps:
-            os.system(f"winget rm {remove}")
+            os.system(f'winget rm "{app}"')
     elif search:
-        os.system(f"winget search {search}")
+        os.system(f'winget search "{search}"')
     else:
         os.system("winget update --all")
 
@@ -25,12 +27,10 @@ def cli(install, update, search, remove):
 
 
 def string_to_array(string):
-    if ',' in string:
-        apps = string.split(",")
-    else:
-        apps = string.split(" ")
+    apps = string.split(" ")
+    for x in range(len(apps)):
+        if '-' in apps[x]:
+            apps[x] = apps[x].replace('-', ' ')
     return apps
-
-
 if __name__ == '__main__':
     cli()
